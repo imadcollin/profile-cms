c48fcfc06d02c66f001ca2200faf479ac4c7a7b5
imadcollin <imad.collin@gmail.com>
2020-12-04 16:48:44 +0100

    Add links as constant

---
+++ src/constants/links.js
@@ -0,0 +1,35 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const Links = () => {
+  const links = [
+    {
+      id: 1,
+      url: "/",
+      text: "home",
+    },
+    {
+      id: 2,
+      url: "/projects/",
+      text: "projects",
+    },
+    {
+      id: 3,
+      url: "/contributors/",
+      text: "contributors",
+    },
+  ]
+  return (
+    <div>
+      {links.map(link => {
+        return (
+          <li key={link.id}>
+            <Link to={link.url}>{link.text}</Link>
+          </li>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Links
